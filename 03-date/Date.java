c72130d5c62dec785e8c7303cb7fcdac6a0631cd
Student <student@ac1mwk19jjc9h.78b505.nycboe.org>
2022-10-03 13:04:34 -0400

    added date files

---
+++ 03-date/Date.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class Date here.
+ *
+ * @imron
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class Date
+
+
+{
+ public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    System.out.print("Enter day of the week");
+    String day = s.nextLine();
+    System.out.println(day);
+    
+    System.out.print("Enter month ");
+    String day1 = s.nextLine();
+    System.out.println(day1);
+    
+    System.out.print("Enter day of the month");
+    String day2 = s.nextLine();
+    System.out.println(day2);
+    
+    System.out.print("Enter year");
+    String year = s.nextLine();
+    System.out.println(year);
+    
+    System.out.println("American Format: "  + day +" "+ day2 +" "+ day2 +" 
+    "+ year +");
+    
+    System.out.println("European Format: " + "+ day +" "+ day2 +" "+ day1 +"
+    "+ year +"
+}
+    
+    
+}
+}
+    
+    
+  
